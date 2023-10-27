3e78279bba9719414a8a06d67622ba9a511bb849
aniket0742 <aniket.nex07@gmail.com>
2023-10-28 02:05:02 +0530

    added files

--- src/JavaCollection/Stack.java
+++
@@ -1,5 +0,0 @@
-package JavaCollection;
-
-public class Stack {
-
-}
--- src/JavaCollection/Stackk.java
+++ src/JavaCollection/Stackk.java
@@ -1,7 +1,13 @@
 package JavaCollection;
-
+import java.util.Stack;
 public class Stackk {
     public static void main(String[] args) {
-
+        // last in that out
+        Stack<String> game = new Stack<>();
+        game.push("Battlefield V");
+        game.push("RDR2");
+        game.push("FIFA 23");
+        System.out.println(game.peek());
+        game.pop();
     }
 }
