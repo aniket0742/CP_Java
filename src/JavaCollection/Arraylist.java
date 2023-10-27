104545dc9cf6a19b92d00eebd9b451b5fafb0a5e
aniket0742 <aniket.nex07@gmail.com>
2023-10-28 00:58:36 +0530

    added files

--- src/JavaCollection/Arraylist.java
+++ src/JavaCollection/Arraylist.java
@@ -1,12 +1,11 @@
 package JavaCollection;
-
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-
 public class Arraylist {
     public static void main(String[] args) {
         // adding elements to array after limit
-        ArrayList<String> students= new ArrayList<>();
+        ArrayList<String> students = new ArrayList<>();
         students.add("Aniket");
         System.out.println(students);
         students.add("Naman");
@@ -19,5 +18,19 @@ public class Arraylist {
         System.out.println(students);
         students.remove(3);
         students.remove("WWW");
+        String s = students.get(3);
+        students.set(3,"tyt");
+        System.out.println(newlis.contains("WWW"));
+        // iterating the list (3 methods)
+        for(int i=0;i<newlis.size();i++){
+            System.out.println(newlis.get(i));
+        }
+        for(String ele:newlis){
+            System.out.println(ele);
+        }
+        Iterator<String> it= newlis.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
     }
 }
